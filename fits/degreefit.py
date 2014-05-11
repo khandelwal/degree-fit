d8e8729f266bca82b635858b8b309520b3aad0ba
Shashank Khandelwal <khandelwal@gmail.com>
2014-05-11 18:48:17 -0400

    Added AIC functions, generic degree fitter, and changed signature of functions to take numpy array instead of python list

--- fits/degreefit.py
+++ fits/degreefit.py
@@ -13,21 +13,18 @@ def read_degree_list(filename):
 def poisson_lambda_estimation(degree_list):
     """ This estimates the parameter lambda for the Poisson distribution 
     given a list of numbers. """
-    return np.array(degree_list).mean()
+    return degree_list.mean()
 
 
 def geometric_lambda_estimation(degree_list):
     """ Estimate the parameter lambda for the Geometric (Exponential)
     distribution"""
-
-    x = np.array([float(k) for k in degree_list])
-    inverse_theta = np.log([x.sum() / (x.sum() - len(x))])[0]
+    inverse_theta = np.log(
+        [degree_list.sum() / (degree_list.sum() - len(degree_list))])[0]
     return 1.0/inverse_theta
 
 
 def poisson_likelihood_term(lambda_estimate, degree_list):
-
-    degree_list = np.array(degree_list)
 
     lambdas = np.empty(len(degree_list))
     lambdas.fill(lambda_estimate)
@@ -42,11 +39,32 @@ def poisson_likelihood_term(lambda_estimate, degree_list):
 
     return numerator / denominator
 
-
 def geometric_likelihood_term(lambda_estimate, degree_list):
     degree_list = np.array(degree_list)
 
     lambdas = np.empty(len(degree_list))
     lambdas.fill(lambda_estimate)
-
     return (1.0/lambdas * np.exp(-((degree_list - 1)/ lambdas)))
+
+def generic_likelihood(degree_list, lambda_estimator):
+    lambda_estimate = lambda_estimator(degree_list)
+    likelihood_terms = likelihood_term_calculator(lambda_estimate, degree_list)
+    return likelihood_terms
+
+
+def akaike_criterion(likelihood_terms):
+    """ Given a numpy array of likelihood terms, compute the AIC. """
+    l_theta = 2.0 * numpy.log(likelihood_terms).sum()
+    return 2 - l_theta + 4.0/(len(likelihood_terms) - 2.0)
+
+
+def degree_list_fitter(degree_list, likelihood_calculators):
+    degree_list = np.array(degree_list, dtype=np.float)
+
+    results = {}
+
+    for calculator_type, calculator in likelihood_calculators.items():
+        likelihood_terms = calculator(degree_list)
+        aic = akaike_criterion(likelihood_tersm)
+        results[calcuator_type] = aic
+    return result
--- fits/degreefit_test.py
+++ fits/degreefit_test.py
@@ -1,27 +1,35 @@
 import unittest
 import degreefit
+import numpy as np
 
 class PoissonTest(unittest.TestCase):
     """ Test all the Poisson distribution related functions. """ 
     def test_poisson_lambda_estimation(self):
-        self.assertEqual(5.0, degreefit.poisson_lambda_estimation([5, 5, 5]))
+        degree_list = np.array([5, 5, 5])
+        self.assertEqual(5.0, degreefit.poisson_lambda_estimation(degree_list))
         self.assertAlmostEqual(
-            2.80, degreefit.poisson_lambda_estimation([1, 2.4, 5]), places=3)
+            2.80,
+            degreefit.poisson_lambda_estimation(
+                np.array([1, 2.4, 5])), places=3)
 
     def test_likelihood(self):
-        lambda_estimate = degreefit.poisson_lambda_estimation([5, 5, 5])
-        result = degreefit.poisson_likelihood_term(lambda_estimate, [5, 5, 5])
+        lambda_estimate = degreefit.poisson_lambda_estimation(
+            np.array([5, 5, 5]))
+
+        result = degreefit.poisson_likelihood_term(
+            lambda_estimate, np.array([5, 5, 5]))
         self.assertEqual(len(result), 3)
         for r in result:
             self.assertAlmostEqual(0.1754, r, places=3)
             
 class GeometricTest(unittest.TestCase):
     def test_geometric_lambda_estimation(self):
+        degree_list = np.array([5, 5, 5, 5], dtype=np.float)
         self.assertAlmostEqual(
-            4.48, degreefit.geometric_lambda_estimation([5, 5, 5, 5]), places=2)
+            4.48, degreefit.geometric_lambda_estimation(degree_list), places=2)
 
     def test_likelihood(self):
-        degree_list = [5, 5, 5, 5]
+        degree_list = np.array([5, 5, 5, 5], dtype=np.float)
         lambda_estimate = degreefit.geometric_lambda_estimation(degree_list)
         result = degreefit.geometric_likelihood_term(
             lambda_estimate, degree_list)
