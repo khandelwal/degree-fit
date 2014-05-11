9ebf8ead2ac03a79323a76012691061155d93fe1
Shashank Khandelwal <khandelwal@gmail.com>
2014-05-11 18:24:38 -0400

    Added likelihood term generators for poisson and geometric

--- fits/degreefit.py
+++ fits/degreefit.py
@@ -1,5 +1,8 @@
 import networkx as nx 
 import numpy as np
+import scipy as sp
+from scipy import special
+
 
 def read_degree_list(filename):
     G = nx.read_edgelist(filename, delimiter=' ', nodetype=int)
@@ -7,17 +10,43 @@ def read_degree_list(filename):
     return degree
 
 
-def poisson_lambda_estimation(num_list):
+def poisson_lambda_estimation(degree_list):
     """ This estimates the parameter lambda for the Poisson distribution 
     given a list of numbers. """
-    x = np.array(num_list)
-    return x.mean()
+    return np.array(degree_list).mean()
 
 
-def geometric_lambda_estimation(num_list):
+def geometric_lambda_estimation(degree_list):
     """ Estimate the parameter lambda for the Geometric (Exponential)
     distribution"""
 
-    x = np.array([float(i) for i in num_list])
+    x = np.array([float(k) for k in degree_list])
     inverse_theta = np.log([x.sum() / (x.sum() - len(x))])[0]
     return 1.0/inverse_theta
+
+
+def poisson_likelihood_term(lambda_estimate, degree_list):
+
+    degree_list = np.array(degree_list)
+
+    lambdas = np.empty(len(degree_list))
+    lambdas.fill(lambda_estimate)
+
+    theta_ds = np.power(degree_list, lambdas)
+
+    exponents = np.empty(len(degree_list))
+    exponents.fill(-lambda_estimate)
+
+    numerator = theta_ds * np.exp(exponents)
+    denominator = special.gamma(degree_list + 1.0)
+
+    return numerator / denominator
+
+
+def geometric_likelihood_term(lambda_estimate, degree_list):
+    degree_list = np.array(degree_list)
+
+    lambdas = np.empty(len(degree_list))
+    lambdas.fill(lambda_estimate)
+
+    return (1.0/lambdas * np.exp(-((degree_list - 1)/ lambdas)))
--- fits/degreefit_test.py
+++ fits/degreefit_test.py
@@ -8,7 +8,23 @@ class PoissonTest(unittest.TestCase):
         self.assertAlmostEqual(
             2.80, degreefit.poisson_lambda_estimation([1, 2.4, 5]), places=3)
 
+    def test_likelihood(self):
+        lambda_estimate = degreefit.poisson_lambda_estimation([5, 5, 5])
+        result = degreefit.poisson_likelihood_term(lambda_estimate, [5, 5, 5])
+        self.assertEqual(len(result), 3)
+        for r in result:
+            self.assertAlmostEqual(0.1754, r, places=3)
+            
 class GeometricTest(unittest.TestCase):
     def test_geometric_lambda_estimation(self):
         self.assertAlmostEqual(
             4.48, degreefit.geometric_lambda_estimation([5, 5, 5, 5]), places=2)
+
+    def test_likelihood(self):
+        degree_list = [5, 5, 5, 5]
+        lambda_estimate = degreefit.geometric_lambda_estimation(degree_list)
+        result = degreefit.geometric_likelihood_term(
+            lambda_estimate, degree_list)
+        self.assertEqual(len(degree_list), len(result))
+        for r in result:
+            self.assertAlmostEqual(0.0913, r, places=3)
